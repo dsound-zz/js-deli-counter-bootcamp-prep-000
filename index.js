8a17f8b4a36d55f574914033f1438c9bbd546454
demian sims <demiansims@gmail.com>
2018-09-17 13:02:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 function takeANumber(line, name) {
  
   for (let i=0; line.length > i; i++) {
-    var lineLength = line[i - 1]
+    
   }
 
   
-  return `Welcome, ${name}. You are number ${lineLength} in line.`
+  return `Welcome, ${name[i]}. You are number ${line[i]} in line.`
   
 }
