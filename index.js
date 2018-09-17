d349588111a5b2be3f87a7548dea7c29058eb479
demian sims <demiansims@gmail.com>
2018-09-17 13:44:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,11 @@ function nowServing(line) {
       return `Currently serving ${line.shift()}.` 
     }
 }
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty."
+    }
+    else {
+      The line is cuurently: ${line[i]}.
+    }
+}
