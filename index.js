bdec34cb32a30de4b12bbe3089b93afa2ad16bdc
demian sims <demiansims@gmail.com>
2018-09-17 13:36:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function nowServing(line) {
     return "There is nobody waiting to be served!"
     }
     else {
-      return `Currently serving ${line.shift()}` 
+      return `Currently serving ${line.shift()}.` 
     }
 }
